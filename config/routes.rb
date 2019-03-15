053b3de4f23da2b2c551d2e493ca32a4185ffcdb
phuonganhdt-1773 <dinh.thi.phuong.anh@framgia.com>
2019-03-15 08:25:40 +0700

    Chap12 - Reset password

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, except: [:index, :show, :destroy]
 end
