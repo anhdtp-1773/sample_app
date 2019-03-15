38ca09a47b166e3f5dd0b2ffb15197b6141d93c1
phuonganhdt-1773 <dinh.thi.phuong.anh@framgia.com>
2019-03-15 14:32:42 +0700

    Chap13 - User microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
-  def home; end
+
+  def home
+    return unless logged_in?
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.page(params[:page])
+      .per Settings.quantity_per_page
+  end
 
   def help; end
 
   def about; end
+
+  def contact; end
 end
